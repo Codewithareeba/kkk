b13485a0c8874115ea964ded7252508e6bb343d7
Umaima145 <umaimamussadiq4@gmail.com>
2024-08-25 17:12:03 +0500

    abs

---
+++ main.dart
@@ -0,0 +1,6 @@
+void main(){
+  String userName = "areeba";
+  String email = "areebaali5@gmail.com";
+  String password = "12334";
+  print(userName == "ayesha" && email =="areebaali5@gmail.com"|| password == "12634" );
+}
